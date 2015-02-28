f6e93c87528179b25d6a09d4d1f29bd5e97aec66
Xin Wei <xinwei91@gmail.com>
2015-02-27 16:07:06 -0800

    finish with home page

--- app/Home/index.jsx
+++ app/Home/index.jsx
@@ -1,13 +1,48 @@
 import React from 'react/addons'
 import {Link} from "react-router"
-
+import {WOW} from "wow/dist/wow"
 import './style.less'
 
-export default React.createClass({
+var Home = React.createClass({
+    wow: null,
     getInitialState() {
         return {
             menuToggle: false
         };
+    },
+    componentWillMount() {
+        this.wow = new WOW().init();
+    },
+    componentDidMount() {
+        // Used to track the enabling of hover effects
+        var enableTimer = 0;
+        /*
+         * Listen for a scroll and use that to remove
+         * the possibility of hover effects
+         */
+        window.addEventListener('scroll', () => {
+          clearTimeout(enableTimer);
+          removeHoverClass();
+
+          // enable after 1 second, choose your own value here!
+          enableTimer = setTimeout(addHoverClass, 1000);
+        }, false);
+
+        /**
+         * Removes the hover class from the body. Hover styles
+         * are reliant on this class being present
+         */
+        function removeHoverClass() {
+          document.body.classList.remove('hover');
+        }
+
+        /**
+         * Adds the hover class to the body. Hover styles
+         * are reliant on this class being present
+         */
+        function addHoverClass() {
+          document.body.classList.add('hover');
+        }
     },
     mixins: [React.addons.PureRenderMixin],
     toggleMenu() {
@@ -63,6 +98,45 @@ export default React.createClass({
                     <div className="heroBg" />
                 </div>
             </div>
+            <div className="container resContainer wow fadeIn" data-wow-duration="3s" data-wow-offset="10">
+                <div className="row-fluid">
+                    <h2> My Work </h2>
+                    <img className="companyLogo wow fadeInLeft" src={require('./assets/trapit-logo.svg')} width="300px" data-wow-duration="3s" />
+                    <p className="light">I&#039;m currently a software engineer at <a href="https://trap.it">Trapit</a>, a spin-off of <a href="http://www.sri.com/">SRI International</a> that uses artifical intelligence for content discovery and curation.</p>
+                    <p className="light center">My current weapons of choice include various modern Javascript MVC Frameworks (Backbone, Ember, Angular), Facebook React (as well as Flux Architecture), Clojurescript (Om and Reagent), Socket.IO, a variety of workflow tools like Gulp, Webpack, Jade Templating, Git, ES6 Harmony, and various unit testing frameworks (Jasmine and Jest). Of course I also enjoy coding server-side languages like Node, PHP, C/C++, and Java.</p>
+                </div>
+            </div>
+            <div className="row-fluid darkBg2 darkBg">
+                <div className="container resContainer wow fadeIn" data-wow-duration="3s">
+                    <h2> Hobbies and Interests </h2>
+                    <p className="dark center">In my free time, I enjoy learning new languages, reading <a href="https://news.ycombinator.com/news">Hacker News</a>, solving Javascript puzzles, working on my side projects, and spending hours on Reddit (too addicting).
+                        <br /><br />
+                        But don&#039;t worry, I also spend plenty of time not near a computer as well! I enjoy hanging out with friends (food adventures, movies, music concerts, snowboarding, hiking, traveling, exploring new areas, board game nights), traveling to different states and countries, and going to various entrepreneurial/tech meet-ups in San Francisco (a good way to learn what other cool startups are doing)! And oh yes I love food adventures ... did I mention that already?
+                    </p>
+                </div>
+            </div>
+            <div className="container resContainer wow fadeIn" data-wow-duration="3s" data-wow-offset="10">
+                <div className="row-fluid">
+                    <h2> Education </h2>
+                    <p className="light center">Bachelor of Science in Computer Science and Engineering</p>
+                    <p className="light center sub">
+                    UCLA School of Engineering and Applied Science
+                    <br />
+                    Class of 2013
+                    <br />
+                    Cumulative GPA 3.6, Major GPA 3.5
+                    <br />
+                    <br />
+                    <img src={require('./assets/ucla.png')} align="center" width="300px" />
+                    </p>
+                </div>
+            </div>
+            <footer className="darkBg">
+                <div className="container resContainer">
+                    <p className="center">Ahh! You reached the bottom of the page already! <a href="#">Go back to top?</a></p>
+                </div>
+            </footer>
         </div>;
     }
 });
+export default Home;
