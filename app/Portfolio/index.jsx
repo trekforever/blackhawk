92c87d8be4e6a9e21c37e4bf824eae6b7624df6f
Xin Wei <xinwei91@gmail.com>
2015-03-15 16:48:36 -0700

    general portfolio

--- app/Portfolio/index.jsx
+++ app/Portfolio/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react/addons'
 import {RouteHandler} from "react-router"
 import {Link} from "react-router"
+import Header from './Header'
+import Filter from './Filter'
+
+import './style.less';
 
 export default React.createClass({
     render() {
-        return <div>
+        return <div className="portfolio">
+            <Header />
+            <Filter />
             <h2>Portfolio</h2>
             <RouteHandler />
         </div>;
