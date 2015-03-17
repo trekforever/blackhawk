a1682463542107dcf05e1712313247cef6e2589e
Xin Wei <xinwei91@gmail.com>
2015-03-16 17:17:06 -0700

    update components to use reflux store and actions

--- app/Portfolio/ProjectList/index.jsx
+++ app/Portfolio/ProjectList/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react/addons'
 import {Link} from "react-router"
+import Spinner from 'Spinner'
+import _ from 'lodash'
 
 export default React.createClass({
+    renderList() {
+        return this.props.projects.map((project) => {
+          var id = _.kebabCase(project.title);
+          return <li><Link to={`/portfolio/projects/${id}`}>{project.title}</Link></li>
+        })
+    },
     render() {
         return <div>
             <h2>Project List</h2>
             <ul>
-                <li><Link to="/portfolio/projects/1">Project 1</Link></li>
-                <li><Link to="/portfolio/projects/41">Project 41</Link></li>
+                {this.renderList()}
             </ul>
         </div>;
     }
--- app/Portfolio/index.jsx
+++ app/Portfolio/index.jsx
@@ -1,18 +1,44 @@
+import Reflux from "reflux"
 import React from 'react/addons'
 import {RouteHandler} from "react-router"
 import {Link} from "react-router"
 import Header from './Header'
 import Filter from './Filter'
+import Spinner from 'Spinner'
+import ProjectList from './ProjectList'
 
-import './style.less';
+import {portfolio as Actions} from 'actions'
+import Store from 'stores/portfolio-store'
+
+import './style.less'
 
 export default React.createClass({
-    render() {
-        return <div className="portfolio">
-            <Header />
-            <Filter />
-            <h2>Portfolio</h2>
-            <RouteHandler />
-        </div>;
+  componentDidMount() {
+    Actions.load();
+  },
+  storeChangeListener(delta) {
+    this.setState({
+      projects: delta
+    });
+  },
+  getInitialState() {
+    return {
+      projects: (void 0)
+    };
+  },
+  renderList() {
+    if(!this.state.projects) {
+      return <Spinner />;
     }
+    return <ProjectList projects={this.state.projects} />
+  },
+  mixins: [Reflux.listenTo(Store, "storeChangeListener")],
+  render() {
+      return <div className="portfolio">
+          <Header />
+          <Filter />
+          { this.renderList() }
+          <RouteHandler />
+      </div>;
+  }
 });
