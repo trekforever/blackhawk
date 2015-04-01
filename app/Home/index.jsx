f86d1d270abeb059fcf37487aeee6715c96dba78
Xin Wei <xinwei91@gmail.com>
2015-03-31 21:11:38 -0700

    fix link hrefs

--- app/Home/index.jsx
+++ app/Home/index.jsx
@@ -80,14 +80,14 @@ var Home = React.createClass({
                             <img className="profile" src={require('./assets/thumb.jpg')} />
                         </div>
                         <h2> Hello there! </h2>
-                        <p className="light center"> My name is Xin Wei, and I&#039;m a software engineer living in San Jose, CA. I love creating simple, clean designs for web and mobile, and use the latest web technologies to craft responsive, interactive and user-friendly websites. </p>
+                        <p className="light center"> {`My name is Xin Wei, and I'm a software engineer living in San Jose, CA. I love creating simple, clean designs for web and mobile, and use the latest web technologies to craft responsive, interactive and user-friendly websites.`} </p>
                         <br />
                         <ul className="socialLinks">
-                            <li><a className="https://www.linkedin.com/in/trekforever"><i className="fa fa-linkedin" /></a></li>
-                            <li><a className="https://twitter.com/trekforever"><i className="fa fa-twitter" /></a></li>
-                            <li><a className="https://plus.google.com/u/0/111807495882361387546/"><i className="fa fa-google-plus" /></a></li>
-                            <li><a className="https://github.com/trekforever"><i className="fa fa-github" /></a></li>
-                            <li><a className="http://stackoverflow.com/users/1547196/trekforever"><i className="fa fa-stack-overflow" /></a></li>
+                            <li><a href="https://www.linkedin.com/in/trekforever"><i className="fa fa-linkedin" /></a></li>
+                            <li><a href="https://twitter.com/trekforever"><i className="fa fa-twitter" /></a></li>
+                            <li><a href="https://plus.google.com/u/0/111807495882361387546/"><i className="fa fa-google-plus" /></a></li>
+                            <li><a href="https://github.com/trekforever"><i className="fa fa-github" /></a></li>
+                            <li><a href="http://stackoverflow.com/users/1547196/trekforever"><i className="fa fa-stack-overflow" /></a></li>
                         </ul>
                         <br />
                         <p className="subheader">San Jose · San Francisco · Web Developer · Software Engineer</p>
